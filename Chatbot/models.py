85a54f2fad704e6499a2ee3bb6a6fd3fa676f8a6
Ishita <iagarwal7@gmail.com>
2018-04-05 14:45:06 +0530

    Added models

--- Chatbot/models.py
+++ Chatbot/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# blueprint for database
+class Restaurant(models.Model):          #generates primary key on its own
+    rating = models.CharField(max_length=10)      #rating, name are all columns in database
+    name = models.CharField(max_length=500)
+    contact = models.CharField(max_length=10)
+    address = models.CharField(max_length=1000)
+
+    def __str__(self):          #string representation of the object
+        return self.name + ' - ' + str(self.rating)
+    
+class Menu(models.Model):
+    date = models.CharField(max_length=15)
+    #dish_id = models.
+    review = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+
