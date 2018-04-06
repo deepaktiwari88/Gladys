d286ce2e71e0dcdb573abd89b5982a5fc71caac8
Deepak Tiwari <tiwarid99@gmail.com>
2018-04-06 21:19:04 +0530

    Added validation for name

--- about/static/about/js/main.js
+++ about/static/about/js/main.js
@@ -7,3 +7,10 @@ function stopScroll() {
     clearTimeout(scrolldelay);
 }
 
+function validateForm(){
+    var x = document.forms["searchform"]["searchbox"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
