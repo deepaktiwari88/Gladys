9d6069d58f1dc1f599d2e89598c18ab251aa930c
Ishita <iagarwal7@gmail.com>
2018-04-09 18:05:11 +0530

    Changed views to fetch values from database

--- Chatbot/views.py
+++ Chatbot/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Restaurant
+
 from django.shortcuts import render
 
 def chat(request):
     return render(request, 'Chatbot/chat.html', {})
 
+def servequery(request):
+    query = request.POST.get('send-request', None)
+    context = {}
+
+    if query=='restaurant' and request.method == 'POST':
+        results = Restaurant.objects.all()
+        context.update({'results':results})
+
+    return render(request, 'Chatbot/chat.html', context)
 
