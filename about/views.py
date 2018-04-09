b3e99832d1b64ad525df66d9a74560b85b10928c
Deepak Tiwari <tiwarid99@gmail.com>
2018-04-09 23:22:17 +0530

    Used session variables to use Username in different views

--- about/views.py
+++ about/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render,redirect
 
 def index(request):
     name = request.POST.get('UserName', None)
+    context={}
+
     if name and request.method == 'POST':
-        context = {'name':name}
-        return render(request,'Chatbot/chat.html', context)
+        request.session['Name'] = name
+        return redirect('chatbot:Chatbot')
 
     else:
         return render(request, 'about/about us.html')
