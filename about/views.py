d1a4b896abe241d8c1cf53999cb7d478ead465b7
Ishita <iagarwal7@gmail.com>
2018-04-09 18:06:08 +0530

    Changed about views to fix same URL problem

--- about/views.py
+++ about/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 def index(request):
     name = request.POST.get('UserName', None)
     if name and request.method == 'POST':
         context = {'name':name}
-        return render(request, 'Chatbot/chat.html', context)
+        return render(request,'Chatbot/chat.html', context)
 
     else:
         return render(request, 'about/about us.html')
