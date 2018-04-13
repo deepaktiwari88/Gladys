e47fce8ee84fb82985097c210bb68235b49ad6b8
Deepak Tiwari <tiwarid99@gmail.com>
2018-04-13 15:37:10 +0530

    Merge branch 'url-database' of https://github.com/ishita-agarwal/Gladys-1 into ishita-agarwal-url-database
    # Conflicts:
    #	Chatbot/__pycache__/models.cpython-36.pyc
    #	Chatbot/__pycache__/urls.cpython-36.pyc
    #	Chatbot/__pycache__/views.cpython-36.pyc
    #	Chatbot/templates/Chatbot/restaurant.html
    #	Chatbot/views.py
    #	db.sqlite3
    
    Signed-off-by: Deepak Tiwari <tiwarid99@gmail.com>

--- Chatbot/admin.py
+++ Chatbot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Address, Menu, Restaurant, Cuisine, User, Order, DeliveryService
+from .models import Address, Menu, Restaurant, Cuisine, User, Order, DeliveryService, Chat
 # Register your models here.
 
 admin.site.register(Address)
@@ -9,3 +9,4 @@ admin.site.register(Cuisine)
 admin.site.register(User)
 admin.site.register(Order)
 admin.site.register(DeliveryService)
+admin.site.register(Chat)
