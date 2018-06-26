0552764137d233b71af8cbee1625218645e6f8b2
Deepak Tiwari <tiwarid99@gmail.com>
2018-06-26 19:26:03 +0530

    Added STATIC_ROOT

--- Gladys/settings.py
+++ Gladys/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "about/static"),
                     os.path.join(BASE_DIR, "Chatbot/static"),
                     ]
